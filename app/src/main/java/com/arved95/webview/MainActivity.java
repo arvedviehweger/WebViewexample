be4251dd05dcba5bffed88bd69b1809da3b7da0b
haimann951 <arved95@gmail.com>
2015-08-19 19:39:11 +0200

    Initial commit

---
+++ app/src/main/java/com/arved95/webview/MainActivity.java
@@ -0,0 +1,27 @@
+package com.arved95.webview;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webview = (WebView) findViewById(R.id.webView);
+        webview.loadUrl("http://atlantis-network.orgfree.com");
+        webview.getSettings().setJavaScriptEnabled(true);
+
+    }
+
+
+}
